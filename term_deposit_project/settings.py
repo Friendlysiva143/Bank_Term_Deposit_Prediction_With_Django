507f1afe5f81428acc6f4cf821c6b93cdc91ba3e
Friendlysiva143 <friendlysiva143@gmail.com>
2025-05-25 09:24:11 +0530

    hosts

--- term_deposit_project/settings.py
+++ term_deposit_project/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'bank-term-deposit-prediction-with-django.onrender.com',
+]
